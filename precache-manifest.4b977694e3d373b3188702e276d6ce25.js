f99f71dadb505793dde9cddaeb2490a40b4dd136
tejas_lad <tejaslad2706@gmail.com>
2020-09-22 23:04:51 +0530

    Updates

---
+++ precache-manifest.4b977694e3d373b3188702e276d6ce25.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f68afe85269048441bf733b173c8bdb",
+    "url": "/printBags/index.html"
+  },
+  {
+    "revision": "c66a513160ae12772787",
+    "url": "/printBags/static/css/main.b070c39d.chunk.css"
+  },
+  {
+    "revision": "828d9dc73102ff29491c",
+    "url": "/printBags/static/js/2.2c6c8fee.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/printBags/static/js/2.2c6c8fee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c66a513160ae12772787",
+    "url": "/printBags/static/js/main.495c4c9b.chunk.js"
+  },
+  {
+    "revision": "a76dca05b437bcbfd773",
+    "url": "/printBags/static/js/runtime-main.1ffdea7e.js"
+  },
+  {
+    "revision": "b694926e2f9932ff13b2802f6498fa18",
+    "url": "/printBags/static/media/img1.b694926e.png"
+  },
+  {
+    "revision": "8afd18e64f5d3f816b588b842b1e4030",
+    "url": "/printBags/static/media/img2.8afd18e6.png"
+  }
+]);
